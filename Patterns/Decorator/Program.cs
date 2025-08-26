4335bc44b27bc48db7c1a50a944da029c07d4e72
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-08-26 07:35:49 +0300

    1.0.4

--- Patterns/Decorator/Program.cs
+++ Patterns/Decorator/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://www.dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 12 мая 2025 03:31:02
- * Version: 1.0.7
+ * Last Updated: 26 августа 2025 07:35:17
+ * Version: 1.0.8
  */
 
 /*  Декоратор
