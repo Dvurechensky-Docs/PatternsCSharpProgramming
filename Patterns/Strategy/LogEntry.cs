4312f0f212597666fc4bce8ceeb7c5aabc4fe55b
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-08-28 07:08:07 +0300

    Auto-update headers via pre_commit.ps1 - 2025-08-28

--- Patterns/Strategy/LogEntry.cs
+++ Patterns/Strategy/LogEntry.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://www.dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 28 августа 2025 06:57:08
- * Version: 1.0.10
+ * Last Updated: 28 августа 2025 07:07:56
+ * Version: 1.0.11
  */
 
 namespace Strategy;
