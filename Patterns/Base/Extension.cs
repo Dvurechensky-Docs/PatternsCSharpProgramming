55d70647fd8235ea42474cc7bab7c18568d35938
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-08-29 06:52:07 +0300

    Auto-update headers via pre_commit.ps1 - 2025-08-29

--- Patterns/Base/Extension.cs
+++ Patterns/Base/Extension.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://www.dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 28 августа 2025 07:07:56
- * Version: 1.0.11
+ * Last Updated: 29 августа 2025 06:51:59
+ * Version: 1.0.12
  */
 
 namespace Base
