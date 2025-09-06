39a09db33309ee22ec3e1470d9e058d13294a593
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-09-06 11:46:26 +0300

    Auto-update headers via pre_commit.ps1 - 2025-09-06

--- Patterns/LetCode/Program.cs
+++ Patterns/LetCode/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://www.dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 05 сентября 2025 10:10:05
- * Version: 1.0.19
+ * Last Updated: 06 сентября 2025 11:46:18
+ * Version: 1.0.20
  */
 
 public static class LetCodeTasks
