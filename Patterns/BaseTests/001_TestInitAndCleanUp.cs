cc6945a3b780e470247d6506737e67fc32b12b0c
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-09-08 13:12:23 +0300

    Auto-update headers via pre_commit.ps1 - 2025-09-08

--- Patterns/BaseTests/001_TestInitAndCleanUp.cs
+++ Patterns/BaseTests/001_TestInitAndCleanUp.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://www.dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 07 сентября 2025 10:48:30
- * Version: 1.0.21
+ * Last Updated: 08 сентября 2025 13:12:15
+ * Version: 1.0.22
  */
 
 using System.Diagnostics;
