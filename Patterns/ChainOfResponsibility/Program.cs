49651665d797a28d612c8493ccb8a9baf1fd1a7c
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-10-16 10:58:14 +0300

    Auto-update headers via pre_commit.ps1 - 2025-10-16

--- Patterns/ChainOfResponsibility/Program.cs
+++ Patterns/ChainOfResponsibility/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://www.dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 15 октября 2025 10:49:40
- * Version: 1.0.59
+ * Last Updated: 16 октября 2025 10:58:07
+ * Version: 1.0.60
  */
 
 /*  Цепочка обязанностей
