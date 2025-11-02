1da4c6a937cf2588197c94834f7c69ccacc4ed91
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-11-02 17:23:19 +0300

    Auto-update headers via pre_commit.ps1 - 2025-11-02

--- Patterns/Base/AssertMsTest.cs
+++ Patterns/Base/AssertMsTest.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://www.dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 01 ноября 2025 06:51:49
- * Version: 1.0.76
+ * Last Updated: 02 ноября 2025 17:23:12
+ * Version: 1.0.77
  */
 
 namespace Base;
