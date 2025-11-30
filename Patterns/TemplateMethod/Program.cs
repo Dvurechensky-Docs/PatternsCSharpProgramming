d3c6d899981b29ecbdbd849267f3b3aace735b53
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-11-30 14:33:08 +0300

    Auto-update headers via pre_commit.ps1 - 2025-11-30

--- Patterns/TemplateMethod/Program.cs
+++ Patterns/TemplateMethod/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://www.dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 28 ноября 2025 09:08:19
- * Version: 1.0.103
+ * Last Updated: 30 ноября 2025 14:33:04
+ * Version: 1.0.104
  */
 
 /*  Шаблонный метод  
