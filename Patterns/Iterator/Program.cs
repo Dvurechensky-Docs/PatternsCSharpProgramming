eb30a5aec29d8fc07341c5e161e279662e64b8ff
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-12-03 11:20:27 +0300

    Auto-update headers via pre_commit.ps1 - 2025-12-03

--- Patterns/Iterator/Program.cs
+++ Patterns/Iterator/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://www.dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 02 декабря 2025 13:36:18
- * Version: 1.0.107
+ * Last Updated: 03 декабря 2025 11:20:20
+ * Version: 1.0.108
  */
 
 /*  Итератор
