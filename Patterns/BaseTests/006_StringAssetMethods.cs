a0b90a5935823a23b3637b30ff6b31fa7e658914
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-12-06 15:35:05 +0300

    Auto-update headers via pre_commit.ps1 - 2025-12-06

--- Patterns/BaseTests/006_StringAssetMethods.cs
+++ Patterns/BaseTests/006_StringAssetMethods.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://www.dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 05 декабря 2025 14:56:23
- * Version: 1.0.110
+ * Last Updated: 06 декабря 2025 15:34:58
+ * Version: 1.0.111
  */
 
 using System.Text.RegularExpressions;
