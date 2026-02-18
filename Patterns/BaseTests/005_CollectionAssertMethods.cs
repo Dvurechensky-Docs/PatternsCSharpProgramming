a158e0ef080bda543d23338edb588df668bb7260
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-02-18 13:24:11 +0300

    Auto-update headers via pre_commit.ps1 - 2026-02-18

--- Patterns/BaseTests/005_CollectionAssertMethods.cs
+++ Patterns/BaseTests/005_CollectionAssertMethods.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://www.dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 17 февраля 2026 09:26:20
- * Version: 1.0.184
+ * Last Updated: 18 февраля 2026 13:24:04
+ * Version: 1.0.185
  */
 
 namespace Base.Tests;
