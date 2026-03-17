94f468f4db1786d28b110521edb573d3e8923697
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-03-17 06:52:17 +0300

    Auto-update headers via pre_commit.ps1 - 2026-03-17

--- Patterns/Base/Program.cs
+++ Patterns/Base/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://www.dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 15 марта 2026 06:52:01
- * Version: 1.0.210
+ * Last Updated: 17 марта 2026 06:52:09
+ * Version: 1.0.211
  */
 
 using Newtonsoft.Json;
