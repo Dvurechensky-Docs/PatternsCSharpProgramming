e9c5ce9fb0b142d186c3a2d7c16f587e68262756
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-09 10:57:35 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-09

--- Patterns/Decorator/Program.cs
+++ Patterns/Decorator/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 08 апреля 2026 14:27:06
- * Version: 1.0.234
+ * Last Updated: 09 апреля 2026 10:57:27
+ * Version: 1.0.235
  */
 
 /*  Декоратор
