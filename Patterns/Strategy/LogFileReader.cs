4d60cb26635cf70213a71173d29131b654ff2da9
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-18 14:44:04 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-18

--- Patterns/Strategy/LogFileReader.cs
+++ Patterns/Strategy/LogFileReader.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 17 апреля 2026 06:51:17
- * Version: 1.0.243
+ * Last Updated: 18 апреля 2026 14:43:56
+ * Version: 1.0.244
  */
 
 namespace Strategy;
