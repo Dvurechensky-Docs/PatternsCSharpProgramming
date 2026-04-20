a87d38279799a2a9202d49dd4c4ec0d813b9287b
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-20 16:21:24 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-20

--- Patterns/Strategy/Program.cs
+++ Patterns/Strategy/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 20 апреля 2026 03:06:06
- * Version: 1.0.245
+ * Last Updated: 20 апреля 2026 16:21:15
+ * Version: 1.0.246
  */
 
 /*  Стратегия  
