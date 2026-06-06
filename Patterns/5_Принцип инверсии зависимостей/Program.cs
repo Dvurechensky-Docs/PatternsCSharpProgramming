23ff610174d8834860b9deee2e1f525877e5c118
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-06 08:47:22 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-06

--- Patterns/5_Принцип инверсии зависимостей/Program.cs
+++ Patterns/5_Принцип инверсии зависимостей/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 05 июня 2026 06:51:40
- * Version: 1.0.294
+ * Last Updated: 06 июня 2026 08:47:14
+ * Version: 1.0.295
  */
 
 /*
