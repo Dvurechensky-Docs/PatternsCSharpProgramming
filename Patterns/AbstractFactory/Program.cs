81efdd719aa55159c686c453f012149250fc961c
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-08 06:51:44 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-08

--- Patterns/AbstractFactory/Program.cs
+++ Patterns/AbstractFactory/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 07 июня 2026 18:20:54
- * Version: 1.0.296
+ * Last Updated: 08 июня 2026 06:51:36
+ * Version: 1.0.297
  */
 
 /*  Абстрактная фабрика
