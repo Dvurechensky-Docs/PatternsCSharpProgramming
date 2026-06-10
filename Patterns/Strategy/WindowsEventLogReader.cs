cd1a5c4b207ae1e6cc9f20717e0a69f2c9c31167
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-10 06:51:52 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-10

--- Patterns/Strategy/WindowsEventLogReader.cs
+++ Patterns/Strategy/WindowsEventLogReader.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 09 июня 2026 16:52:43
- * Version: 1.0.298
+ * Last Updated: 10 июня 2026 06:51:43
+ * Version: 1.0.299
  */
 
 namespace Strategy;
