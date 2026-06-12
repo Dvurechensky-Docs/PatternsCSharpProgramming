86e3fcac7dd616775bc14351a26470180a37226f
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-12 06:51:58 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-12

--- Patterns/Mediator/Program.cs
+++ Patterns/Mediator/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 11 июня 2026 13:25:35
- * Version: 1.0.300
+ * Last Updated: 12 июня 2026 06:51:49
+ * Version: 1.0.301
  */
 
 /*  Посредник
