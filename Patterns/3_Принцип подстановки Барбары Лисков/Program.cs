c907b70f5ab931c0f3edd46eaf03f037dc0711d6
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-13 14:00:25 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-13

--- Patterns/3_Принцип подстановки Барбары Лисков/Program.cs
+++ Patterns/3_Принцип подстановки Барбары Лисков/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 12 июня 2026 06:51:49
- * Version: 1.0.301
+ * Last Updated: 13 июня 2026 14:00:16
+ * Version: 1.0.302
  */
 
 /*
