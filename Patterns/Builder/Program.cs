6481f9bbfcc93831f6be4e573402efd348432073
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-26 06:51:49 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-26

--- Patterns/Builder/Program.cs
+++ Patterns/Builder/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 25 июня 2026 16:29:55
- * Version: 1.0.314
+ * Last Updated: 26 июня 2026 06:51:41
+ * Version: 1.0.315
  */
 
 /*  Строитель
