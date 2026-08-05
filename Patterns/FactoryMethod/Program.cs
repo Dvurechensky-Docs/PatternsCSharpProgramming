67faec63c1bbf35fd4f38bb1b0e41bee4b0e3588
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-05 06:51:55 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-05

--- Patterns/FactoryMethod/Program.cs
+++ Patterns/FactoryMethod/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 04 августа 2026 06:52:07
- * Version: 1.0.355
+ * Last Updated: 05 августа 2026 06:51:46
+ * Version: 1.0.356
  */
 
 /*  Фабричный метод
