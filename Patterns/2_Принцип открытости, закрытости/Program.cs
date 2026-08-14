a829e71cdefc53669a0b0747e3eeeafca7b51823
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-14 08:16:01 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-14

--- Patterns/2_Принцип открытости, закрытости/Program.cs
+++ Patterns/2_Принцип открытости, закрытости/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 13 августа 2026 06:51:45
- * Version: 1.0.364
+ * Last Updated: 14 августа 2026 08:15:52
+ * Version: 1.0.365
  */
 
 /*
