07387f887877545797d2b573b1ea077bbf1400da
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-15 06:52:02 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-15

--- Patterns/Bridge/Program.cs
+++ Patterns/Bridge/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 14 августа 2026 08:15:52
- * Version: 1.0.365
+ * Last Updated: 15 августа 2026 06:51:52
+ * Version: 1.0.366
  */
 
 /*  Мост
