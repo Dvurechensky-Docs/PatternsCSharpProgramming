e4c40c336a2479f1f1dd8df0ff254913d2b89d8e
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-19 08:45:13 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-19

--- Patterns/Proxy/Program.cs
+++ Patterns/Proxy/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 18 сентября 2026 08:13:09
- * Version: 1.0.400
+ * Last Updated: 19 сентября 2026 08:45:03
+ * Version: 1.0.401
  */
 
 using Microsoft.EntityFrameworkCore;
