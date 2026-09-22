0382e026689bf19a69133ae52e13e8186f6e4aba
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-22 09:17:46 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-22

--- Patterns/BaseTests/007_ExpectingExceptions.cs
+++ Patterns/BaseTests/007_ExpectingExceptions.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 21 сентября 2026 09:33:54
- * Version: 1.0.403
+ * Last Updated: 22 сентября 2026 09:17:37
+ * Version: 1.0.404
  */
 
 namespace Base.Tests;
